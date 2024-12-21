f546c7fbcc59d43c7120457793e50e19ef07b968
DuyTinNguyen182 <119142567+DuyTinNguyen182@users.noreply.github.com>
2024-12-21 07:34:14 +0700

    Update and rename test.js to process-mediapipe.js

--- process-mediapipe.js
+++ process-mediapipe.js
@@ -78,7 +78,7 @@ hands.onResults((results) => {
           // Xử lí cho ngón cái - khối gạch qua phải
           console.log(`Toa do X: ${landmarks[finger_id[0]].x}`);
           console.log(`Toa do X -2: ${landmarks[finger_id[0] - 2].x}`);
-          if (landmarks[finger_id[0]].x + 0.01 < (landmarks[finger_id[0] - 2].x))
+          if (landmarks[finger_id[0]].x < (landmarks[finger_id[0] - 2].x - 0.015))
             fingers[4] = 1;
 
           // Xử lí cho ngón út - khối gạch qua trái
